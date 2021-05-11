8f69cfd781215093a855f5fe54943ffdd0f811ca
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-05-11 09:50:28 -0400

    sovanhong

---
+++ help.js
@@ -0,0 +1,11 @@
+const arr = [2, 4, 5, 8];
+
+function sumArrayOfNums(arr, index = 0, sum = 0) {
+  if (index === arr.length) {
+    return sum;
+  }
+  sum += arr[index];
+  return sumArrayOfNums(arr, index + 1, sum);
+}
+
+console.log(sumArrayOfNums);
